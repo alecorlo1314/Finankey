151570291ba6230a597f30dd280549cad677099b
alecorlo <alecorlo1234@gmail.com>
2025-07-31 22:31:17 -0600

    Mejoras en la vista detalle cuenta

--- FinanKey/ViewModels/ViewModelDetalleCuenta.cs
+++ FinanKey/ViewModels/ViewModelDetalleCuenta.cs
@@ -7,6 +7,6 @@ namespace FinanKey.ViewModels
     public partial class ViewModelDetalleCuenta : ObservableObject
     {
         [ObservableProperty]
-         Cuenta cuenta;
+        private Cuenta cuenta;
     }
 }
