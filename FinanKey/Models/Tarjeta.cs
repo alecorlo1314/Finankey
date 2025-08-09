315dd34212118bf9817ac9efb7546e8006e626e2
alecorlo <alecorlo1234@gmail.com>
2025-08-09 17:28:02 -0600

    Se agrego un nueva clase llamada Tarjeta tanto para la base de datos como para modelos

---
+++ FinanKey/Models/Tarjeta.cs
@@ -0,0 +1,34 @@
+﻿using SQLite;
+
+namespace FinanKey.Models
+{
+    [Table("Tarjeta")]
+    public class Tarjeta
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [Indexed, NotNull]
+        public string Alias { get; set; } = string.Empty;
+
+        [NotNull]
+        public string Tipo { get; set; } = "Credito"; // "Credito" | "Debito" | "Corriente"
+
+        [MaxLength(4)]
+        public string UltimosCuatroDigitos { get; set; } = string.Empty;
+
+        public string Color { get; set; } = "#37265A"; // default color hex
+
+        public decimal? LimiteCredito { get; set; }
+
+        public decimal SaldoPendiente { get; set; } = 0m; // Solo para tarjetas de crédito
+
+        public decimal SaldoActual { get; set; } = 0m; // Para tarjetas de débito y corrientes
+
+        public int? DiaCorte { get; set; }
+        public int? DiaPago { get; set; }
+
+        public bool EsPredeterminada { get; set; } = false;
+
+        public DateTime FechaCreacion { get; set; } = DateTime.UtcNow;
+    }
+}
