47a07f2d3767135b32571d01dfd0cc0801ac5a80
alecorlo <alecorlo1234@gmail.com>
2025-08-09 18:48:32 -0600

    Se agrego El Servicio de base datos para Tarjeta basados en la interfaz IServicioTarjeta

--- FinanKey/Servicios/IServicioTarjeta.cs
+++ FinanKey/Servicios/IServicioTarjeta.cs
@@ -4,8 +4,8 @@ namespace FinanKey.Servicios
     public interface IServicioTarjeta
     {
         public Task<int> AgregarAsync(Tarjeta Nuevatarjeta);
-        public Task<int> ActualizarAsync(Tarjeta TarjetaActualizada);
-        public Task<int> EliminarAsync(int idTarjeta);
+        public Task ActualizarAsync(Tarjeta TarjetaActualizada);
+        public Task EliminarAsync(int idTarjeta);
         public Task<List<Tarjeta>> ObtenerTarjetasAsync();
         public Task<Tarjeta?> ObtenerTarjetaPorIdAsync(int idTarjeta);
     }
