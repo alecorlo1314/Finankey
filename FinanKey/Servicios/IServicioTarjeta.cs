3bb00ab345e78db01e57b28e8e6e3d5cfaff7b11
alecorlo <alecorlo1234@gmail.com>
2025-08-09 18:19:21 -0600

    Se agrego un interfaz llamda IServicioTarejeta, que contendra los servicios para la clase ServicioTarjeta

---
+++ FinanKey/Servicios/IServicioTarjeta.cs
@@ -0,0 +1,12 @@
+﻿using FinanKey.Models;
+namespace FinanKey.Servicios
+{
+    public interface IServicioTarjeta
+    {
+        public Task<int> AgregarAsync(Tarjeta Nuevatarjeta);
+        public Task<int> ActualizarAsync(Tarjeta TarjetaActualizada);
+        public Task<int> EliminarAsync(int idTarjeta);
+        public Task<List<Tarjeta>> ObtenerTarjetasAsync();
+        public Task<Tarjeta?> ObtenerTarjetaPorIdAsync(int idTarjeta);
+    }
+}
