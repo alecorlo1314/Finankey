22a184102c61cc16ffc2548d25ee2b9d159a45fe
alecorlo <alecorlo1234@gmail.com>
2025-08-20 22:02:48 -0600

    Cambios

--- FinanKey/Models/Enums.cs
+++ FinanKey/Models/Enums.cs
@@ -13,5 +13,11 @@ namespace FinanKey.Models
             Ingreso = 0,
             Gasto = 1,
         }
+        public enum MarcaTarjeta
+        {
+            Visa = 0,
+            Mastercard = 1,
+            American_Express = 2,
+        }
     }
 }
