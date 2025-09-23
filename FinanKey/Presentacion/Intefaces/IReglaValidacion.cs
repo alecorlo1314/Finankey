dc436d23f7cdf6e9e463f8a7fa6d904d70e79eee
alecorlo <alecorlo1234@gmail.com>
2025-09-22 21:58:52 -0600

    Se agregaron cambios a la clase de validacion y se agrego un interfa llamada IReglaVlaidacion

---
+++ FinanKey/Presentacion/Intefaces/IReglaValidacion.cs
@@ -0,0 +1,8 @@
+﻿
+namespace FinanKey.Presentacion.Intefaces;
+
+public interface IReglaValidacion<T> 
+{
+    string ValidandoMensaje { get; set; }
+    bool Revisar(T valor);
+}
--- FinanKey/Presentacion/View/Validaciones/ObjetoValidable.cs
+++ FinanKey/Presentacion/View/Validaciones/ObjetoValidable.cs
@@ -1,7 +1,43 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-
+using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace FinanKey.Presentacion.View.Validaciones;
 
-class ObjetoValidable
+class ObjetoValidable<T> : ObservableObject, IValidity
 {
+    #region PROPIEDADES
+    //contiene los errores de validacion
+    [ObservableProperty]
+    private IEnumerable<string> _listaErrores;
+
+    //decide si el objetos es valido o no
+    [ObservableProperty]
+    private bool _esValido;
+
+    [ObservableProperty]
+    private T _value;
+
+    //contiene las reglas de validacion
+    public List<ReglasValidacion<T>> Validaciones { get; } = new();
+    #endregion
+
+    #region CONSTRUCTOR
+    public ObjetoValidable()
+    {
+        EsValido = true;
+        ListaErrores = new Enumerable.Empty<string>();
+    }
+    #endregion
+
+    #region METODO DE VALIDACION
+    public bool Validar()
+    {
+        ListaErrores = Validaciones
+            ?.Where(v => !v.Check(Value))
+            ?.Select(v => v.ValidationMessage)
+            ?.ToArray()
+            ?? Enumerable.Empty<string>();
+        EsValido = !ListaErrores.Any();
+        return EsValido;
+    }
+    #endregion
 }
