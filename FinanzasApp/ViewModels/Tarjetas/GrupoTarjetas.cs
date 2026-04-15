811da801ed676c6aadf5efe9534bb3a6c1715c09
alecorlo <alecorlo1234@gmail.com>
2026-04-14 22:52:50 -0600

    Se realizaon cambios en la ui y se agrego el archivo readme

---
+++ FinanzasApp/ViewModels/Tarjetas/GrupoTarjetas.cs
@@ -0,0 +1,36 @@
+﻿using FinanzasApp.Aplicacion.DTOs;
+
+namespace FinanzasApp.Presentacion.ViewModels.Tarjetas;
+
+public class GrupoTarjetas : List<TarjetaResumenDto>
+{
+    /// <summary>
+    /// Etiqueta del grupo
+    /// Ejemplo (Debito,Credito)
+    /// </summary>
+    public string TipoTarjeta { get; }
+
+    /// <summary>
+    /// Cantidad de tarjetas en el grupo
+    /// Ejemplo (2,3)
+    /// </summary>
+    public string Etiqueta { get; }
+
+    /// <summary>
+    /// Subtitulo del grupo
+    /// Ejemplo (tarjetas, tarjetas)
+    /// </summary>
+    public string Subtitulo { get; }
+
+    public GrupoTarjetas(
+        string tipoTarjeta, 
+        string etiqueta, 
+        string subtitulo, 
+        List<TarjetaResumenDto> tarjetas)
+        : base(tarjetas)
+    {
+        TipoTarjeta = tipoTarjeta;
+        Etiqueta = etiqueta;
+        Subtitulo = subtitulo;
+    }
+}
